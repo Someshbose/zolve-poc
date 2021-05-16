6bc2a515df9bed5802c7093120f91d1502108a1e
pujaburman30 <pujaburman30@gmail.com>
2021-05-16 20:35:29 +0530

    initial test

---
+++ src/main/java/pujaburman30github/io/zolvepoc/ZolvePocApplication.java
@@ -0,0 +1,13 @@
+package pujaburman30github.io.zolvepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZolvePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZolvePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pujaburman30github/io/zolvepoc/ZolvePocApplicationTests.java
@@ -0,0 +1,13 @@
+package pujaburman30github.io.zolvepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZolvePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

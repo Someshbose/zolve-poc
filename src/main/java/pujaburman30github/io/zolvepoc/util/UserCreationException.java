960f048c346527809852edbbfc6b880311731304
Someshbose <iamsomeshbose@gmail.com>
2021-05-20 12:48:35 +0530

    exception changes & transaction history changes added

---
+++ src/main/java/pujaburman30github/io/zolvepoc/util/UserCreationException.java
@@ -0,0 +1,8 @@
+package pujaburman30github.io.zolvepoc.util;
+
+public class UserCreationException extends RuntimeException{
+
+    public UserCreationException(String doc){
+        super(doc);
+    }
+}
---
+++ src/main/java/pujaburman30github/io/zolvepoc/util/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package pujaburman30github.io.zolvepoc.util;
+
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException(String doc) {
+        super(doc);
+    }
+}
